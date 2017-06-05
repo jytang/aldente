9ceaee325e173d278110ba4de3e6d06ae886e65a
Joshua Tang <joshua.y.tang@gmail.com>
2017-06-05 05:10:27 -0700

    address etans comments

--- src/game/game_state.cpp
+++ src/game/game_state.cpp
@@ -105,14 +105,10 @@ void GameState::set_phase(Phase* phase) {
         curr_phase->s_setup();
     }
     else {
-        if (curr_phase == nullptr) {
-            curr_phase = phase;
-            curr_phase->c_setup();
-        } else {
+        if (curr_phase)
             curr_phase->c_teardown();
-            curr_phase = phase;
-            curr_phase->c_setup();
-        }
+        curr_phase = phase;
+        curr_phase->c_setup();
     }
 }
 
